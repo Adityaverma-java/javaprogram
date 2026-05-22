9ee0dee1767138930aa0f52a48681f0b29887312
Adityaverma-java <adityaverma123456679@gmail.com>
2026-05-22 18:03:54 +0530

    changes in java B

--- javaB.java
+++ javaB.java
@@ -1,5 +1,6 @@
 public class javaB {
     public static void main(String[] args) {
         System.out.println("you can java developer");
+        System.out.println("helloooooooooooo.....");
     }
 }
